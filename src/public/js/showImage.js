863566ddd4f9713fc1b70cb96b282c96cada148a
Lee-Si-Yoon <76679207+Lee-Si-Yoon@users.noreply.github.com>
2021-10-29 00:00:10 +0900

    before Heroku

--- src/public/js/showImage.js
+++ src/public/js/showImage.js
@@ -1,23 +1,32 @@
 const socket = io();
 
-const btn_container = document.querySelector("#btn_container");
-const btn = btn_container.querySelector("button");
 const img = document.images[0];
 const bigImg = document.createElement("img");
+const btn_container = document.querySelector("#btn_container");
 
 //import img1 from "../imgs/snuman.gif"; // ../../../assets/imgs/1.jpg
 import img2 from "../imgs/snuman.webp";
 import img3 from "../imgs/ballon.png";
 //import img3 from "../imgs/3.jpg";
 
+bigImg.onload = function () {
+  img.src = this.src;
+  img.className = "noBlur";
+};
+setTimeout(function () {
+  bigImg.src = "/assets/imgs/snuman.webp";
+}, 50);
+
+btn_container.style.display = "none";
+
 function getRandomImage() {
-  bigImg.onload = function () {
-    img.src = this.src;
-    img.className = "noBlur";
-  };
+  btn_container.style.display = "flex";
   setTimeout(function () {
-    bigImg.src = "/assets/imgs/snuman.webp";
-  }, 50);
+    btn_container.classList.add("hide");
+  }, 3000);
+  setTimeout(function () {
+    //btn_container.style.display = "none";
+  }, 4000);
 }
 
 socket.on("show_message", getRandomImage);
--- src/server.js
+++ src/server.js
@@ -26,11 +26,19 @@ const getHome = (req, res) => {
 
 const postHome = async (req, res) => {
   const { name } = req.body;
+  const exists = await Graduate.exists({ name });
+  if (exists) {
+    return res.status(400).render("home", {
+      pageTitle: "Home",
+      errorMessage:
+        "이름이 중복됩니다. 동명이인일 경우 이름 옆에 숫자를 붙여주세요",
+    });
+  }
   try {
     await Graduate.create({
       name: name,
     });
-    return res.redirect("/cert");
+    return res.render("cert", { name: name, pageTitle: "Cert" });
   } catch (error) {
     console.log(error);
     return res.redirect("/");

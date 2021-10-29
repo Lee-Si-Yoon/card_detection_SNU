13c6326cf5233c75d319c91bd9dcf802ae6c74f4
Lee-Si-Yoon <76679207+Lee-Si-Yoon@users.noreply.github.com>
2021-10-30 03:26:44 +0900

    showimg pls2

--- src/public/js/showImage.js
+++ src/public/js/showImage.js
@@ -10,21 +10,20 @@ import img3 from "../imgs/snu_logo.png";
 import img4 from "../imgs/top_dude.png";
 //import img3 from "../imgs/3.jpg";
 
-img_shower.style.display = "none";
-unload.style.display = "block";
+//img_shower.style.display = "none";
+//unload.style.display = "block";
 
-img_shower.addEventListener("load", () => {
-  unload.style.display = "none";
-  img_shower.style.display = "block";
-});
+//img_shower.addEventListener("load", () => {
+//  unload.style.display = "none";
+//  img_shower.style.display = "block";
+//});
 
-btn_container.style.display = "none";
 function getRandomImage() {
   //unload.style.display = "none";
   btn_container.style.display = "flex";
   setTimeout(function () {
     btn_container.classList.add("hide");
-  }, 5000);
+  }, 3000);
   setTimeout(() => {
     location.reload();
   }, 1000);
